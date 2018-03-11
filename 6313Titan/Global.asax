f9a6425aa8e697af94f4cf1d03f5b805a752bd1c
kingbeazer <beattie.edwin@gmail.com>
2018-03-11 20:55:12 +0000

    Initial Commit

---
+++ 6313Titan/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_6313Titan.MvcApplication" Language="C#" %>
---
+++ 6313Titan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_6313Titan.Startup))]
+namespace _6313Titan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
