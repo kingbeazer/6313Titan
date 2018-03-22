72fa144c38f4fcd47bdb588ebe79a257b93e277f
Ryan Stuart <ryan_7229@hotmail.co.uk>
2018-03-22 21:03:14 +0000

    AddedAdministrationPage

---
+++ 6313Titan/Controllers/AdministrationController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _6313Titan.Models;
+
+namespace _6313.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public AdministrationController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Administration
+        public ActionResult Index(Guid PortalId)
+        {
+            ViewBag.data = PortalId;
+            return View();
+        }
+    }
+}
